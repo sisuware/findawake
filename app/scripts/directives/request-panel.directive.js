9b95bbdd05d15203233fdf5393e042986c625262
Kyle Simukka <kyle.simukka@elementaltechnologies.com>
2015-07-19 00:29:44 -0700

    updated styles on request panel directive

--- app/scripts/directives/request-panel.directive.js
+++ app/scripts/directives/request-panel.directive.js
@@ -10,7 +10,7 @@
   function requestPanel(Users, Requests) {
     var _html  = '<div class="thumbnail" ng-class="{\'color3\':request.accepted,\'color4\':request.declined,\'highlight\':!request.accepted && !request.declined}">';
         _html += '  <a href="/profile/{{request.userId}}" target="_blank"><img avatar="{{profile.avatar}}" /></a>';
-        _html += '   <h3 class="text-center"><a ng-href="/profile/{{request.userId}}" target="_blank">{{profile.name}}</a></h3>';
+        _html += '   <h4 class="text-center"><a ng-href="/profile/{{request.userId}}" target="_blank">{{profile.name}}</a></h4>';
         _html += '  <div class="caption text-center">';
         _html += '    <span class="label {{request.expenses ? \'label-success\':\'label-warning\'}}">${{request.expenses || \'0\'}}.<sup>00</sup></span>';
         _html += '  </div>';
@@ -31,8 +31,8 @@
         _html += '    <button class="btn btn-primary" role="button" ng-click="acceptRequest(request)">Accept</button>';
         _html += '  </div>';
         _html += '  <div ng-cloak ng-show="acceptedOrDeclined()" class="caption text-center">';
-        _html += '    <button ng-cloak ng-show="request.accepted" class="btn btn-default color3b" ng-disabled="request.accepted">Accepted</button>';
-        _html += '    <button ng-cloak ng-show="request.declined" class="btn btn-default color4b" ng-disabled="request.declined">Declined</button>';
+        _html += '    <span ng-cloak ng-show="request.accepted" class="text-success">Request Accepted</span>';
+        _html += '    <span ng-cloak ng-show="request.declined" class="text-danger">Request Declined</span>';
         _html += '  </div>';
         _html += '</div>';
 
