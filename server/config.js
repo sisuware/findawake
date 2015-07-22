861c4fbf3a06d872b2f4236bbd6d45493ee17496
Kyle Simukka <simook@gmail.com>
2015-07-21 20:53:00 -0700

    added google credentials and npm package

--- server/config.js
+++ server/config.js
@@ -18,5 +18,11 @@ module.exports = {
   moment: {
     dateFormat: 'dddd, MMMM Do',
     timeFormat: 'h:mm a' 
+  },
+
+  google: {
+    clientId: '78864326005-9u9ckmkl5t72o35dnr9ehaea3ml9plve.apps.googleusercontent.com'.
+    clientEmail: '78864326005-9u9ckmkl5t72o35dnr9ehaea3ml9plve@developer.gserviceaccount.com',
+    clientKey: '-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDUy6qzqEqaBwSz\nof4/IjBzh7PLuSCaPdGbRKbAeLIwOREKxdK5k9BQllGI57sIPk7m4D79SYvw7C+h\nsAyF90k7s15yWwHNk6ElsJl+0Ta4DIzxdLUuk3/4y7Qqny4E8aN4i9mnWemCJCsg\nOrTiaNPPzjUOUhuUyi+F+7djuspHi6qsMTVjtz92FD7BLMY3YgOfAotBENxNoj+l\nTCKGlzm/btQp5m10m8SHhCEvA31F9UDUChWP2u9AA/846sV9Vz6DWZQ6flf1Xn4X\npZy/+OdA1ROHnwH4t2gCrY9Go9IO2QsjDDqENP4Ff8SV9yRHWh/gxpyt4KxhP12j\ncR3T5gMLAgMBAAECggEBAKmYSMw27H4l6dFg4kle7k6lcN6FBT7o1g1xgZUUnK9i\nS/rR/0h4TiZsJz99sMq5HhlTz9/+pdG166BptxFuLKewJS3u6lj1uBFZ1nwEMiAt\noIcwBNbCJ9QaRdVSWFMIYsjIbQe72ScnvFdLcq6f7XBJYG+xcNn86jNWkqqhTGYH\nrkz7cFobZT0Y4K+oLzULL+aGk6hXcUkULddyX+HHrHZfKeTmzF26IfiQiG9Cmbqq\nQcQubJgno6NtX9yox/Gxo8TTWZJMPtiopZJyBEP6EyHSIbkYa/VrkGTHflc1BuZI\n/e85Fi0bn1yp3a7Da2at/Lq1qj/5ZGbTQEqc/dYzCgECgYEA6XpeRq/HOKKMG/39\nqEUDQYwsYGyc3CBskUgRPTTJoX1fwDlhPwqHnXCb3zudP/N3ZPgHpblCo4zgXhx1\nrafq5GVZsH46F7ckkMyRUwwl+dVo/67o5ItjifrX3cBrToMab/5tXq05REI5eeQv\ncBdbRCxXrUetQ93iDGCMb/HRviECgYEA6VKONf+D9857o4Y/eRVpcCf6YuewnGKq\nN7KquZ6d9q89di3XPa8YWX/6F9cxV0DT5wCrbUHkG89JaU5KLNaDa3SUMD66WEs1\nCM2mRZhioqCurWo/SOvSA/vgV9uubTgmaZ7DdEG0B0dnuePYb49s0x1OuD4mXoN7\n3biJH1cdo6sCgYEA3adonV7/mYrnzSNijz+FidMTxi8W8/xsVbi/dQYKbh/iqVFQ\nO1M50IQ51sJ31WDEdrzqNbEdzLtPvsjyB30lCNQXQ/FyIVWjhzxOszMTUo4CcsqR\nG3ghdbsI+ONWfGEG1lALcRivB1h/zHKPfrEN2jjmvAESQx4BHpMCgLxozUECgYBy\nSr1c2CBl+CB6dt2Ky6FNjlfJsSLDSFWHWR7mNDu+xhK+XDAuiCHOXbE9km0aB6iF\nZL+Uko1RzhVw3yfYlO/laELa6y9O5vBAsrQ7LS1L6pgGXKBJcTL8Ppqjjvn/iFzQ\nqjOmNDUix6iY13KDG2e8doPEHrWe4JuBfc61qm/bZwKBgQClcaGcyFJsd1HmM/Yv\n+xxCfLsLuwAMupCHjXujbNL4txc04qi2QgCNdF8PCFFa49ptUf/ejVCa22+/LWG9\nf4yTit3apV/gEIZcf6SNbHlz9CEkb7gp9Zxmds57aouQuJXxU+3XaOdz3qi4RSnj\nPAuH9hqbKc9kt2Jz978ao5lMSw\u003d\u003d\n-----END PRIVATE KEY-----\n'
   }
 };
